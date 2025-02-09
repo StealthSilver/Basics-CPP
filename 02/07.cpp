8ae6851e00162f56ce2d9bd2237929d158b1fa8a
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-09 10:57:49 +0530

    01

---
+++ 02/07.cpp
@@ -0,0 +1,15 @@
+// inputs in cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cout << "enter the age : ";
+    cin >> age;
+    cout << "your age is" << age << endl;
+
+    return 0;
+}
