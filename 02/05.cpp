c0e3d16175b1fcc237273d77a7b858c5e70b6e76
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-09 10:55:01 +0530

    01

---
+++ 02/05.cpp
@@ -0,0 +1,14 @@
+// type conversion
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    char grade = 'b';
+
+    int value = grade;
+    cout << value << endl;
+    return 0;
+}
---
+++ 02/06.cpp
@@ -0,0 +1,14 @@
+// type casting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double price = 100.01;
+
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+    return 0;
+}
