fde108c4e2dea2341035a82ab01340e3c2df983d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-09 11:28:17 +0530

    01

--- 02/14.cpp
+++ 02/14.cpp
@@ -10,5 +10,12 @@ int main()
 
     cout << "b = " << b << endl;
     cout << "a = " << a << endl;
+
+    int c = 8;
+    int d = c++;
+
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+
     return 0;
 }
