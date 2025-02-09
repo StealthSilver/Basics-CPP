2467e9a761fdad0df5cee1a05a5d2a3cef236825
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-09 11:00:17 +0530

    01

---
+++ 02/08.cpp
@@ -0,0 +1,14 @@
+// sum of two numebrs as the input
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    cout << "enter a & b: ";
+    cin >> a >> b;
+    cout << "the sum of" << a << " and " << b << " is " << a + b << endl;
+    return 0;
+}
