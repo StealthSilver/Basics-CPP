557bc5952d7b3e219ae05639d88ba8a3aa51c2e4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 19:34:31 +0530

    03

---
+++ 05/09.cpp
@@ -0,0 +1,29 @@
+// pass by value
+
+#include <iostream>
+using namespace std;
+
+int sumDigit(int a)
+{
+
+    int sum = 0;
+
+    while (a > 0)
+    {
+        int lastDigit = a % 10;
+        a /= 10;
+
+        sum = lastDigit;
+    }
+
+    return sum;
+}
+
+int main()
+{
+
+    int a = 1456;
+    cout << sumDigit(a) << endl;
+
+    return 0;
+}
