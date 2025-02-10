a303b266c3d0f531a5e520f84c5fec81dde58201
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 18:31:37 +0530

    01

--- 04/06.cpp
+++ 04/06.cpp
@@ -5,20 +5,20 @@ int main()
 {
     int n = 3;
 
-    int num = 1;
+    char ch = 'A';
 
     for (int i = 0; i < n; i++)
     {
 
         for (int j = 0; j < n; j++)
         {
-            cout << num << " ";
-            num++;
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
 
-    cout << "aftre pattern : " << num;
+    cout << "aftre pattern : " << ch;
 
     return 0;
 }
