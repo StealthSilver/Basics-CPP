94c56b1340e67d23e2611b8e90ede5bc33c12040
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 19:02:17 +0530

    01

--- 04/16.cpp
+++ 04/16.cpp
@@ -8,18 +8,14 @@ int main()
 {
 
     int n = 4;
-
     // top
-
     for (int i = 0; i < n; i++)
     {
-
         // spaces
         for (int j = 0; j < n - i - 1; j++)
         {
             cout << " ";
         }
-
         cout << "*";
 
         if (i != 0)
