04df6446408b841424061524a2c301c3c63bc25e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 18:46:29 +0530

    01

---
+++ 04/12.cpp
@@ -0,0 +1,26 @@
+// inverted triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        { // loop for printing spaces
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++) // loop for printing numbers
+        {
+            cout << "i+1" << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
