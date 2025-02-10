119f1b2090b5396318c50e9fc85441e0dd945ac5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 18:34:04 +0530

    01

--- 04/07.cpp
+++ 04/07.cpp
@@ -8,10 +8,11 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i + 1; j++)
         {
-            cout << i << " ";
+            cout << "*" << " ";
         }
+        cout << endl;
     }
 
     return 0;
