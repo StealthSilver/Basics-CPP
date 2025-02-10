8a7f605b407badbe18fb950db2c32d0231d892e3
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 12:00:20 +0530

    01

--- 03/14.cpp
+++ 03/14.cpp
@@ -8,9 +8,34 @@ int main()
 {
 
     int n;
+    bool isPrime = true;
 
     cout << "enter the number" << endl;
     cin >> n;
+
+    if (n < 2)
+    {
+        cout << "Not prime" << endl;
+    }
+
+    for (int i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
+
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime == false)
+    {
+        cout << "not prime" << endl;
+    }
+    else
+    {
+        cout << "prime" << endl;
+    }
 
     return 0;
 }
