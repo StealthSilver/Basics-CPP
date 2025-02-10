b49842fd025937fbfdd5692b05d0ceba40cdeacb
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 12:07:27 +0530

    01

---
+++ 03/16.cpp
@@ -0,0 +1,22 @@
+// sum of all numbers upto n which are div of 3
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, sum = 0;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 3 == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    cout << sum << endl;
+    return 0;
+}
