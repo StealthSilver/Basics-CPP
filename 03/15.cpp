c8aff08ebb433e821725579bc03f61eaee93d116
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 12:03:10 +0530

    01

---
+++ 03/15.cpp
@@ -0,0 +1,19 @@
+// nested loops
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << "* " << endl;
+        }
+    }
+
+    return 0;
+}
