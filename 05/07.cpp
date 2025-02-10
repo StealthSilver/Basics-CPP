e9c014401d4870885e47ab5e9aded4dcb84dfa12
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 19:29:26 +0530

    03

--- 05/07.cpp
+++ 05/07.cpp
@@ -1,1 +1,25 @@
+// pass by value
 
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b)
+{
+
+    a = a + 10;
+    b = b + 2;
+
+    return a + b;
+}
+
+int main()
+{
+
+    int a = 5, b = 7;
+    cout << sum(a, b) << endl;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
