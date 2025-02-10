c901ed4c2fb24405b38b59fa791f90d73c935fc1
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 19:16:17 +0530

    03

---
+++ 05/05.cpp
@@ -0,0 +1,21 @@
+// function to find sum of two nums
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int n)
+
+{
+
+    int sum = n * (n + 1) / 2;
+
+    return sum;
+}
+
+int main()
+{
+    cout << sum(3) << endl;
+
+    return 0;
+}
