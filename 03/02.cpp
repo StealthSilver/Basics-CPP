4105796abca352d2a1634f6520f6d70759c27c51
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 11:19:51 +0530

    01

---
+++ 03/02.cpp
@@ -0,0 +1,21 @@
+// even or odd number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+
+    cin >> a;
+
+    if (a % 2 == 0)
+    {
+        cout << "even" << endl;
+    }
+    else
+    {
+        cout << "odd" << endl;
+    }
+}
