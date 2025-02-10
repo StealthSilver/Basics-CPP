25c0ee96608c9a0e4bdb1072d40aa54f41a802f9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 11:32:17 +0530

    01

---
+++ 03/06.cpp
@@ -0,0 +1,19 @@
+// while loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 0;
+
+    while (a <= 100)
+    {
+        cout << a << endl;
+
+        a++;
+    }
+
+    return 0;
+}
