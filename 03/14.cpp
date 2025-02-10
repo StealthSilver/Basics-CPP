407641f96d6f6197d8430b027dd405320688a373
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 11:50:28 +0530

    01

---
+++ 03/14.cpp
@@ -0,0 +1,16 @@
+// checking if a numebr is prime
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "enter the number" << endl;
+    cin >> n;
+
+    return 0;
+}
