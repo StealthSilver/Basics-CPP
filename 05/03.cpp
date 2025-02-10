0397bf2f051078c62386dd07a8cf0726f4f69f3e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 19:11:15 +0530

    03

---
+++ 05/03.cpp
@@ -0,0 +1,19 @@
+// function to find sum of two nums
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int a, int b)
+{
+    int ans = a + b;
+
+    return ans;
+}
+
+int main()
+{
+    cout << sum(3, 7) << endl;
+
+    return 0;
+}
