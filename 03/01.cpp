7d7d11fcf8b65014bf526cfcba25080183a850ab
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 11:17:48 +0530

    01

---
+++ 03/01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "eligible for voting" << endl;
+    }
+    else
+    {
+        cout << "not eligible for voting" << endl;
+    }
+
+    return 0;
+}
