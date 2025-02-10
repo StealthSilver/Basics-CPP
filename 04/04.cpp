e70dc9c037f5dacf5b47dcc765aee8051775cba4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-10 18:23:38 +0530

    01

---
+++ 04/04.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+
+    for (int i = 1; i <= n; i++) // outer loop -> no of lines
+    {
+        char ch = 'A';
+        for (int j = 1; j <= n; j++) // inner loop starts -> line starts
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
