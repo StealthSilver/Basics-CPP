2e072a48880f03b602859f5ef7253d4e3698cb3d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-18 09:07:46 +0530

    init

--- 07/09.cpp
+++ 07/09.cpp
@@ -6,6 +6,8 @@ int main()
 
     cout << sizeof(int) << endl;
     cout << sizeof(long int) << endl;
+    cout << sizeof(long long int) << endl;
+    cout << sizeof(short int) << endl;
 
     return 0;
 }
