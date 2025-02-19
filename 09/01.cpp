e2e2b8ff8139907b524c784414f369cc42e68962
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 13:08:16 +0530

    basics of vectors

---
+++ 09/01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+// # include <bits/c++.h> this contains all the components from STL
+using namespace std;
+
+int main()
+{
+    vector<int> vec;
+
+    return 0;
+}
