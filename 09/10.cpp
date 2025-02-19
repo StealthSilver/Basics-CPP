f1e774a02c6f4d9141b1cdac66953d17932b08bc
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 18:24:43 +0530

    vectors

--- 09/10.cpp
+++ 09/10.cpp
@@ -1,2 +1,26 @@
 // single number (leetcode 136)
 // XOR method
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    int result = 0;
+
+    for (int num : nums)
+    {
+        result ^= num; // XOR each number
+    }
+
+    return result; // The single number remains
+}
+
+int main()
+{
+    vector<int> nums = {4, 1, 2, 1, 2};
+    cout << "Single Number: " << singleNumber(nums) << endl;
+    return 0;
+}
