a93d33430d490d1385804b67317968da5be52643
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 18:33:39 +0530

    vectors

--- 09/12.cpp
+++ 09/12.cpp
@@ -1,1 +1,33 @@
 // reversing a vector
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void reverseVector(vector<int> &nums)
+{
+    int left = 0, right = nums.size() - 1;
+
+    // reverse(nums.begin(), nums.end()); // STL function
+    while (left < right)
+    {
+        swap(nums[left], nums[right]);
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+
+    reverseVector(nums);
+
+    for (int num : nums)
+    {
+        cout << num << " ";
+    }
+
+    return 0;
+}
