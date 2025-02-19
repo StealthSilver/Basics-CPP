cad1e069a70ed3750581998bfd5f5b5ab9493282
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 18:44:44 +0530

    vectors

---
+++ 10/02.cpp
@@ -0,0 +1,30 @@
+// brute force approch for max sum (leetcode 56)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int maxSum = INT_MIN;
+
+    for (int st = 0; st < n; st++)
+    {
+        int currSum = 0;
+
+        for (int end = st; end < n; end++)
+        {
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
+        }
+        cout << endl;
+    }
+
+    cout << "max subarray sum = " << maxSum << endl;
+
+    return 0;
+}
