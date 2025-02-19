1265c729459000b4eb5a16bf0f844e9c22566506
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 10:27:49 +0530

    init

--- 08/04.cpp
+++ 08/04.cpp
@@ -1,1 +1,31 @@
-// smallest and largest element in an array
+// smallest element in an array
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int marks[5];
+    int size = 5;
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> marks[i];
+    }
+
+    int smallest = INT_MAX;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (marks[i] < smallest)
+        {
+            smallest = marks[i];
+        }
+    }
+
+    cout << smallest << endl;
+
+    return 0;
+}
