62dc1fd2be8c49273cea1d6c1db3928ab702a984
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 13:53:29 +0530

    vectors

---
+++ 09/02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> num(5, 0);
+    cout << num[0] << endl;
+    cout << num[1] << endl;
+    cout << num[2] << endl;
+    cout << num[3] << endl;
+    cout << num[4] << endl;
+
+    return 0;
+}
