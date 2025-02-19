7588f8c2fcd6537c71a9b7e6583f83a7c1058f0f
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-19 10:16:41 +0530

    init

---
+++ 08/01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int marks[5] = {22, 3, 45, 32, 23};
+    double price[] = {43, 23, 12};
+
+    cout << marks << endl;
+    cout << price << endl;
+
+    return 0;
+}
