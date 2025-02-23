b5bebf59da96461c3cbf62fb93ce94bd62ac840d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-23 10:56:03 +0530

    tests

--- 10/06.cpp
+++ 10/06.cpp
@@ -1,4 +1,4 @@
-// brute force for majority element
+// brute force for majority element -> O(n^2)
 
 #include <iostream>
 #include <vector>
@@ -6,6 +6,26 @@ using namespace std;
 
 int main()
 {
+    vector<int> nums = {2, 7, 11, 15, 15, 15, 15, 15};
+
+    int n = nums.size();
+
+    for (int val : nums)
+    {
+        int freq = 0;
+
+        for (int el : nums)
+        {
+            if (el == val)
+            {
+                freq++;
+            }
+        }
+        if (freq > n / 2)
+        {
+            return val;
+        }
+    }
 
     return 0;
 }
