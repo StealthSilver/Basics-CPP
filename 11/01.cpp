aa94699b0fb424d6af6726477a722810fd6ee067
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-27 12:10:54 +0530

    time comp

---
+++ 11/01.cpp
@@ -0,0 +1,30 @@
+// prime number -> O(sqrt(n))
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a prime number." << endl;
+    else
+        cout << num << " is not a prime number." << endl;
+
+    return 0;
+}
